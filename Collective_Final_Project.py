a207d4e581617642cb6ab706b844499f77222a7b
JenDionicio <111029322+JenDionicio@users.noreply.github.com>
2024-04-29 16:55:24 +0200

    Update Collective_Final_Project.py

--- Collective_Final_Project.py
+++ Collective_Final_Project.py
@@ -181,7 +181,7 @@ elif app_mode == "Prediction":
   X = tech_df.drop(columns="NetProfitMargin_ratio")  
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
   lin_reg = LinearRegression()
-  # lin_reg.fit(X_train,y_train)
+  lin_reg.fit(X_train,y_train)
   # pred = lin_reg.predict(X_test)
 
   # plt.figure(figsize=(10,7))
