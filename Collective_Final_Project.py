b524fe2b498b8d8f7903181673f25ee1a207719f
JenDionicio <111029322+JenDionicio@users.noreply.github.com>
2024-05-01 21:47:52 +0200

    Update Collective_Final_Project.py

--- Collective_Final_Project.py
+++ Collective_Final_Project.py
@@ -3,9 +3,11 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from PIL import Image
+import mlflow
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.tree import DecisionTreeClassifier, plot_tree
