06927a1bc93f0c222dbd9db387e6c5bc50a6eda9
JenDionicio <111029322+JenDionicio@users.noreply.github.com>
2024-05-02 17:45:24 +0200

    Update Collective_Final_Project.py

--- Collective_Final_Project.py
+++ Collective_Final_Project.py
@@ -33,7 +33,7 @@ if app_mode == "Introduction":
   st.image("ESG_image.png", use_column_width=True)
 
 
-  st.markdown("Environmental - Social - Governance")
+  st.markdown("##Environmental - Social - Governance")
   st.markdown("##### Wondering how ESG rankings truly effect company investment & returns?")
   st.markdown("Our goal is explore a companies profit margin ratio relative to ESG Rankings & finding/creating a positive feedback loop ")
   st.markdown("##### Objectives:")
