a1717cfc3836d141722610cbf0939f89d9055277
JenDionicio <111029322+JenDionicio@users.noreply.github.com>
2024-05-02 20:53:21 +0200

    Update Collective_Final_Project.py

--- Collective_Final_Project.py
+++ Collective_Final_Project.py
@@ -95,8 +95,8 @@ if app_mode == "Introduction":
   # Display Missing Values Matrix in the second column
   with col2:
       st.write("Missing Values Matrix:")
-      mno.matrix(tech_df, figsize=(20, 6))
-      st.pyplot()
+      fig, axplt.matrix(tech_df, figsize=(20, 6))
+      st.pyplot(fig)
   
   if total_miss <= 30:
     st.success("This Data set is reliable to use with small amounts of missing values, thus yielding accurate data.")
