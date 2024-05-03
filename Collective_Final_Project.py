d0114374c8db520f1c63171d7ecc0728d36359c1
JenDionicio <111029322+JenDionicio@users.noreply.github.com>
2024-05-03 02:14:47 +0200

    Update Collective_Final_Project.py

--- Collective_Final_Project.py
+++ Collective_Final_Project.py
@@ -221,7 +221,7 @@ elif app_mode == "Visualization":
   tab2.pyplot(fig)
 
    # Create subplots
-  fig, axes = plt.subplots(2, 4, figsize=(12, 6))
+  fig, axes = plt.subplots(2, 4, figsize=(16, 8))
   
   # Plot bar charts
   sns.barplot(x='ESG_ranking', y='Volatility_sell', data=tech_df, ax=axes[0, 0])
