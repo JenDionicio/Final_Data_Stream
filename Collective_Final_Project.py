b96dbb3632c688cc8587825bd3980ca5e1f4802e
JenDionicio <111029322+JenDionicio@users.noreply.github.com>
2024-05-03 03:48:18 +0200

    Update Collective_Final_Project.py

--- Collective_Final_Project.py
+++ Collective_Final_Project.py
@@ -170,7 +170,7 @@ elif app_mode == "Visualization":
   if button:
     tab1.write(messages[1])
     tab1.image(image_paths[1], use_column_width=True)
-    button2 = tab1.button('Next Pair Plot ')
+  button2 = tab1.button('Next Pair Plot ')
   if button2:
     tab1.write(messages[2])
     tab1.image(image_paths[2], use_column_width=True)
